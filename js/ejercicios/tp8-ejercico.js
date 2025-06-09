edf602192b30c6d799e3c003ed2c5e14311c0a50
Naim Paz <naimpaz274@gmail.com>
2025-06-09 03:03:25 -0300

    termine el tp8

---
+++ js/ejercicios/tp8-ejercico.js
@@ -0,0 +1,7 @@
+//Escribe un programa que pida un número y diga si es divisible por 2
+const num = parseInt(prompt("Escribi un numero"))
+if(num % 2 === 0){
+    document.writeln("Tu numero es divisible en dos")
+}else{
+    document.writeln("Tu numero no es divisible en dos")
+}

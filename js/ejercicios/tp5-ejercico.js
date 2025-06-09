3bc00d17e0c0885e727169fd32b6848bcc751773
Naim Paz <naimpaz274@gmail.com>
2025-06-09 02:22:22 -0300

    termine tp5

---
+++ js/ejercicios/tp5-ejercico.js
@@ -0,0 +1,5 @@
+//Escribe un programa de tres líneas que pida un número, pida otro número y
+//escriba el resultado de sumar estos dos números.
+const numero1 = parseInt(prompt("ingrese un numero"));
+const numero2 = parseInt(prompt("ingrese otro numero"));
+document.writeln("La suma del número " + numero1 + " mas el numero  " + numero2 + " es igual a = " + (numero1 + numero2));

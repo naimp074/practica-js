2dc5ddb264968e62c9381599225b11296a7ac778
Naim Paz <naimpaz274@gmail.com>
2025-06-19 00:47:21 -0300

    punto 6 terminado

--- js/tpbucles/ejercicio-5bucles.js
+++ js/tpbucles/ejercicio-5bucles.js
@@ -1,23 +1,92 @@
-const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
-                'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
-
-while (true) {
-  let input = prompt("Introduce un número de DNI (entre 0 y 99999999):");
-
-  if (input === null) {
-    // El usuario tocó "Cancelar"
-    break;
-  }
-
-  let dni = parseInt(input);
-
-  if (isNaN(dni) || dni < 0 || dni > 99999999) {
-    alert("Número inválido. Por favor, introduce un número entre 0 y 99999999.");
-    continue;
-  }
-
-  let resto = dni % 23;
-  let letra = letras[resto];
-
-  alert(`DNI: ${dni} → Letra correspondiente: ${letra}`);
-}
+let resto = 0;
+let letra = "";
+do {
+    const numero = prompt("Ingrese su dni");
+    if (!isNaN(numero) && numero !=null) {  
+        numero= parseInt(numero);
+        if (numero >= 0 && numero <= 99999999) {
+        
+            resto = numero % 23;
+            console.log(resto);
+            switch (resto) {
+                case 0:
+                    letra = "T";
+                    break;
+                case 1:
+                    letra = "R";
+                    break;
+                case 2:
+                    letra = "W";
+                    break;
+                case 3:
+                    letra = "A";
+                    break;
+                case 4:
+                    letra = "G";
+                    break;
+                case 5:
+                    letra = "M";
+                    break;
+                case 6:
+                    letra = "Y";
+                    break;
+                case 7:
+                    letra = "F";
+                    break;
+                case 8:
+                    letra = "P";
+                    break;
+                case 9:
+                    letra = "D";
+                    break;
+                case 10:
+                    letra = "X";
+                    break;
+                case 11:
+                    letra = "B";
+                    break;
+                case 12:
+                    letra = "N";
+                    break;
+                case 13:
+                    letra = "J";
+                    break;
+                case 14:
+                    letra = "Z";
+                    break;
+                case 15:
+                    letra = "S";
+                    break;
+                case 16:
+                    letra = "Q";
+                    break;
+                case 17:
+                    letra = "V";
+                    break;
+                case 18:
+                    letra = "H";
+                    break;
+                case 19:
+                    letra = "L";
+                    break;
+                case 20:
+                    letra = "C";
+                    break;
+                case 21:
+                    letra = "K";
+                    break;
+                case 22:
+                    letra = "E";
+                    break;
+                default:
+                
+                    alert('Ingrese un numero valido');
+                }
+                alert("Numero: " + numero + ", Letra: " + letra);
+        }else{
+            alert('Ingrese un DNI valido');
+        }
+    }else{
+        alert('Ingrese un numero valido');
+    }
+} while(confirm('¿Desea continuar?'))
---
+++ js/tpbucles/ejercicio-6bucles.js
@@ -0,0 +1,8 @@
+  for (let i = 1; i <= 30; i++) {
+ 
+      for (let rep = 0; rep < i; rep++) {
+  
+          document.writeln(i);
+      }
+      document.writeln("<br>");
+  }
